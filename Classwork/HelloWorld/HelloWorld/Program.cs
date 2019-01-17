405e3dc274320706dbe34f4693f4f3bc10b64904
jeffrey.perkins149 <jeffrey.perkins149@NEITD-123513>
2019-01-16 21:44:22 -0600

    example nonsense of first csharp program

---
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Multi line comment
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    // Single line comment
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            string name;
+
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Hello ");
+            Console.WriteLine(name);
+        }
+    }
+}
