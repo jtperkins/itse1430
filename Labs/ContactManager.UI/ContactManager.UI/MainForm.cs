3eb5182945155763ea52891b5660ba6fd5925c3d
JTPerkins <heershey@outlook.com>
2019-03-27 13:34:03 -0500

    MainForm with fileExit and helpAbout

---
+++ Labs/ContactManager.UI/ContactManager.UI/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactManager.UI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void OnAbout(object sender, EventArgs e)
+        {
+            var form = new AboutBox();
+
+            form.ShowDialog();
+        }
+    }
+}
